7b27d7f71688e103323f110a5bae70e06545b412
Mamadu Billo Djalo <mamadubdj@gmail.com>
2022-07-02 04:23:46 +0100

    Jsx

---
+++ Task-Manager/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+//routes
+app.get('/hello', (req, res)=>{
+        res.end('Task Manager App');
+})
+
+const port = 5000;
+app.listen(port, console.log(`Server is listening on port ${port}...`))

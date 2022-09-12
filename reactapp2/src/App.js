24fa0b2a8c1229897df08cfe3ba99a86ba67f9c8
Mamadu Billo Djalo <mamadubdj@gmail.com>
2022-09-12 02:22:49 +0100

    props

--- reactapp2/src/App.js
+++ reactapp2/src/App.js
@@ -1,8 +1,48 @@
 import ExpenseItem from "./components/ExpenseItem";
+//import Expenses from "./components/Expenses";
 function App(){
+
+    const expenses = [
+        {
+            id: 'e1',
+            title: 'Toilett Paper',
+            amount: 94.12,
+            date: new Date(2020, 21, 10)
+        },
+        {
+            id: 'e2',
+            title: 'Car Insurance',
+            amount: 94.12,
+            date: new Date(2010, 5, 12)
+        },
+        {
+            id: 'e3',
+            title: 'New TV',
+            amount: 94.12,
+            date: new Date(2022, 5, 11)
+        }
+    ]
+
     return (
-        <ExpenseItem/>
+        <div>
+        <ExpenseItem 
+        title={expenses[0].title} 
+        amount={expenses[0].amount} 
+        date={expenses[0].date}>
+        </ExpenseItem>
+
+        <ExpenseItem 
+        title={expenses[1].title}
+         amount={expenses[1].amount}
+         date={expenses[1].date}>
+        </ExpenseItem>
         
+        <ExpenseItem 
+        title={expenses[2].title} 
+        amount={expenses[2].amount} 
+        date={expenses[2].date}>
+        </ExpenseItem>
+        </div>
     )
 }
 export default App;

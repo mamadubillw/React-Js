610aca8652ac5498952c21d48b62053ad7db138e
Mamadu Billo Djalo <mamadubdj@gmail.com>
2022-09-14 05:13:08 +0100

    miltiple state

--- reactapp2/src/components/NewExpense/ExpenseForm.js
+++ reactapp2/src/components/NewExpense/ExpenseForm.js
@@ -1,11 +1,26 @@
+import { useState } from "react"
+
 const ExpenseForm = ()=>{
 
+    const [enteredTitle, setEnteredTitle] = useState('')
+    const [enteredAmount, setEnteredAmount] = useState('')
+    const [enteredDate, setEnteredDate] = useState('')
+
+    const titleChangeHandler = (event) =>{
+        setEnteredTitle(event.target.value)
+    }
+    const amountChangeHandler = (event) =>{
+        setEnteredAmount(event.target.value)
+    }
+    const dateChangeHandler = (event) =>{
+        setEnteredDate(event.target.value)
+    }
     return(
         <form>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text" />
+                    <input type="text"  onChange={titleChangeHandler}/>
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>

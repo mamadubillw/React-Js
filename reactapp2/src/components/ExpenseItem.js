c049e77c83632cf832cbae0a34ee0456c4fd6202
Mamadu Billo Djalo <mamadubdj@gmail.com>
2022-09-14 04:25:11 +0100

    State

--- reactapp2/src/components/ExpenseItem.js
+++ reactapp2/src/components/ExpenseItem.js
@@ -1,19 +1,21 @@
+import ExpenseDate from "./ExpenseDate"
+import { useState } from "react"
 function ExpenseItem(props){
-     const month = props.date.toLocaleString('en-US',{month: 'long'});
-     const day = props.date.toLocaleString('en-US',{day: '2-digit'});
-     const year = props.date.getFullYear();
+    const [title, setTitle] = useState(props.title)
+    const clickHandler = () =>{
+        setTitle('upadted')
+        console.log(title)
+    }
+      
     return(
        <div className="expense-item">
-        <div>
-            <div>{month}</div>
-            <div>{day}</div>
-            <div>{year}</div>
-        </div>
+        <ExpenseDate date={props.date}/>
    
         <div className="expense-item_description">
-            <h2>{props.title}</h2>
+            <h2>{title}</h2>
             <div className="expense-item_price">{props.amount}</div>
         </div>
+        <button onClick={clickHandler}>Change Title</button>
        </div>
     )
 }
